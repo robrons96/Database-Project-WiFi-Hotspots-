0ebac1642b92b01b8557746aa617b814f37dd4c7
l1mb0 <robin_ronson@yahoo.com>
2018-04-13 17:36:13 -0500

    Added Maps API

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,24 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Search } from 'semantic-ui-react'
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 
 class Header extends React.Component {
   constructor(props) {
     super(props);
-  
-
-
   }
 
- 
+
   render() {
     return (
-      <div>`
-      <Search size='large' style = { { float: "left", paddingTop: "20px"} } />
+      <div>
+        <Map style={{width: '100%', height: '100%'}} google={this.props.google} zoom={14}>
+        <Search size='large' style={{ float: "left", paddingTop: "20px", position: "absolute"}} />
+        </Map>
       </div>
     );
   }
 }
 
 
-export default connect()(Header);
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyAQ6YdJQNioPpb3uiJ6JviM_sBJngu9Y4s'
+})(Header)
